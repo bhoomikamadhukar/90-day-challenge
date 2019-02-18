99b9d59ce1c93fd0a9d64fe9366c876a5175e5d1
Bhoomika <bhoomika97@gmail.com>
2019-02-18 21:57:19 +0530

    add question

--- day1_Sockmerchant.py
+++ day1_Sockmerchant.py
@@ -1,3 +1,20 @@
+'''John works at a clothing store. He has a large pile of socks that he must pair by color for sale. 
+Given an array of integers representing the color of each sock, 
+determine how many pairs of socks with matching colors there are.
+
+sockMerchant has the following parameter(s):
+
+n: the number of socks in the pile
+ar: the colors of each sock
+Input Format
+
+The first line contains an integer , the number of socks represented in . 
+The second line contains  space-separated integers describing the colors  of the socks in the pile.
+
+Output Format
+
+Return the total number of matching pairs of socks that John can sell.'''
+
 def sockMerchant(n, ar):
     count=0
     ar.sort()
