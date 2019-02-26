11c9534d0b62a83ef617240fc9e1db12bf41a88d
Bhoomika <bhoomika97@gmail.com>
2019-02-26 18:33:49 +0530

    Add Question

--- day_10_findingpercentage.py
+++ day_10_findingpercentage.py
@@ -1,3 +1,11 @@
+'''You have a record of N students.
+ Each record contains the student's name, and their percent marks in Maths, Physics and Chemistry. 
+ The marks can be floating values. 
+ The user enters some integer N followed by the names and marks for students. 
+ You are required to save the record in a dictionary data type. 
+ The user then enters a student's name. 
+ Output the average percentage marks obtained by that student, correct to two decimal places.'''
+
 n = int(input())
 student_marks = {}
 for _ in range(n):
@@ -8,4 +16,5 @@ for _ in range(n):
 query_name = input()
 query_scores = student_marks[query_name]
 print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))
+
 
