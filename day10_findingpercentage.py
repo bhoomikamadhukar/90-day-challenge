113439f774599018d8dff08c364609fcbe3e8bf1
Bhoomika <bhoomika97@gmail.com>
2019-02-26 18:25:02 +0530

    Find the percentage of marks

---
+++ day10_findingpercentage.py
@@ -0,0 +1,11 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    line = input().split()
+    name, scores = line[0], line[1:]
+    scores = map(float, scores)
+    student_marks[name] = scores
+query_name = input()
+query_scores = student_marks[query_name]
+print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))
+
