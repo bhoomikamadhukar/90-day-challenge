78c26d8a33be9cae4f0747da5eb48702ec53c859
Bhoomika <bhoomika97@gmail.com>
2019-03-21 12:34:59 +0530

    Add day 33

---
+++ day_33_mean_var_std.py
@@ -0,0 +1,6 @@
+import numpy 
+n, m = map(int, input().split())
+array = numpy.array([input().strip().split() for _ in range(n)], int)
+print(numpy.mean(array,axis=1))
+print(numpy.var(array,axis=0))
+print(numpy.std(array))
